16bde8dfdb50dc6d1dbd2b2fb57c1beb85d4cb5d
LinKitten <2634024527@qq.com>
2022-10-22 00:15:37 +0800

    商品热卖

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,37 @@
 // pages/index/index.js
 
-// 导入request请求工具类
-import { requestUtil } from '../../utils/requestUtil.js'
+// 导入request请求工具类 // 导入getBaseUrl
+import {
+  getBaseUrl,
+  requestUtil
+} from '../../utils/requestUtil.js'
+// 引入 facebook的regenerator库
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 Page({
+
 
   /**
    * 页面的初始数据
    */
   data: {
     // 轮播图数组
-    swiperList: []
+    swiperList: [],
+    baseUrl: '',
+    bigTypeList: [],
+    bigTypeList_row1: [],
+    bigTypeList_row2: [],
+    hotProductList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //一开始就先加载请求根路径
+    const baseUrl = getBaseUrl();
+    this.setData({
+      baseUrl
+    })
     //发送异步请求 从后端获取数据
     // wx.request({
     //   url: "http://localhost:8080/product/findSwiper",
@@ -29,13 +45,65 @@ Page({
     //   }
     // })
     // 
-    requestUtil({ url: "http://localhost:8080/product/findSwiper", method: "GET" }).then(result => {
-      // 成功的时候执行 对swiperList赋值
-      this.setData({
-        swiperList: result.message
-      })
+    this.getSwiperList();
+    this.getBigTypeList();
+    this.getHotProductList();
+  },
+  // 轮播图获取数据
+  async getSwiperList() {
+    // requestUtil({ url: "/product/findSwiper", method: "GET" })
+    //   .then(result => {
+    //     const baseUrl = getBaseUrl();
+    //     // 成功的时候执行 对swiperList赋值
+    //     this.setData({
+    //       swiperList: result.message,
+    //       baseUrl
+    //     })
+    //   })
+    const result = await requestUtil({
+      url: "/product/findSwiper",
+      method: "GET"
+    });
+    // 成功的时候执行 对swiperList赋值
+    this.setData({
+      swiperList: result.message,
     })
   },
+  /**
+   * 获取热门商品推荐数据
+   */
+  async getHotProductList() {
+    const result = await requestUtil({
+      url: "/product/findHot",
+      method: "GET"
+    });
+    // 成功的时候执行 对swiperList赋值
+    this.setData({
+      hotProductList: result.message,
+    })
+  },
+
+  //异步函数 金刚区 获取bigTypeList 并初始化row1和row2
+  async getBigTypeList() {
+    const result = await requestUtil({
+      url: "/bigType/findAll",
+      method: "GET"
+    });
+    console.log(result);
+    const bigTypeList = result.message;
+    // 过滤 分别拿到两行的数据
+    const bigTypeList_row1 = bigTypeList.filter((item, index) => {
+      return index < 5;
+    });
+    const bigTypeList_row2 = bigTypeList.filter((item, index) => {
+      return index >= 5;
+    });
+    this.setData({
+      bigTypeList,
+      bigTypeList_row1,
+      bigTypeList_row2
+    })
+  }
 
 
 })
--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -1,10 +1,19 @@
 /**
+ * 定义请求根路径 baseUrl
+ */
+const beasUrl = "http://localhost:8080";
+// 返回请求根路径
+export const getBaseUrl=()=>{
+    return beasUrl;
+}
+/**
  * 后端请求工具类
  */
 export const requestUtil=(params)=>{
     return new Promise((resolve,reject)=>{
         wx.request({
           ...params,//解构url
+          url:beasUrl+params.url,//请求url更改
           success:(result)=>{
               resolve(result.data)
           },
