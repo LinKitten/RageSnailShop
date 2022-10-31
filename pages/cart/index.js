19c677fb31c02d938a5540caece96a110384c39e
LinKitten <2634024527@qq.com>
2022-10-31 23:04:41 +0800

    实现购物车添加、删除、全选、确认地址、结算等功能

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,18 +1,44 @@
 // pages/cart/index.js
+// 导入request请求工具类 // 导入getBaseUrl
+import {
+    getBaseUrl,
+    requestUtil
+} from '../../utils/requestUtil.js'
+// 引入 facebook的regenerator库
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        baseUrl: '',
+        address: {},
+        cart: [],
+        allChecked: false, //全选框状态
+        totalPrice: 0,//购物车商品总数
+        totalNum: 0,//商品总金额
     },
-
+    /**
+     *  获取收货地址
+     */
+    handleChooseAddress() {
+        wx.chooseAddress({
+            success: (result) => {
+                console.log(result);
+                // 存储到缓存中
+                wx.setStorageSync('address', result)
+            },
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const baseUrl = getBaseUrl();
+        this.setData({
+            baseUrl
+        });
     },
 
     /**
@@ -26,41 +52,115 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+        // 从缓存中获取数据，存入data
+        const address = wx.getStorageSync('address');
+        const cart = wx.getStorageSync('cart') || [];//cart为空时设置成一个空数组
 
+        this.setData({
+            address,
+        })
+        this.setCart(cart);
     },
 
     /**
-     * 生命周期函数--监听页面隐藏
+     * 商品选中事件处理
      */
-    onHide: function () {
+    handleItemChange(e) {
+        // console.log(e);
+        const { id } = e.currentTarget.dataset;
+        // console.log(this.data);
+        let { cart } = this.data;
+        let index = cart.findIndex(v => v.id === id);
+        cart[index].checked = !cart[index].checked;//取反
+
+        this.setCart(cart);//重新处理
 
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
+    // 商品全选事件处理
+    handleItemAllCheck() {
+        let { cart, allChecked } = this.data;
+        console.log(cart, allChecked);
+        allChecked = !allChecked;
+        cart.forEach(v => v.checked = allChecked);
+        //   重新计算
+        this.setCart(cart);
     },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+    // 商品数量的编辑功能
+    handleItemNumEdit(e) {
+        const { id, operation } = e.currentTarget.dataset;
+        console.log(id, operation, this.data);
+        let { cart } = this.data;
+        let index = cart.findIndex(v => v.id === id);
+        if (cart[index].num === 1 && operation === -1) {
+            wx.showModal({
+                this:'来自系统的提示',
+                content:'你确定要删除吗？',
+              cancelColor: 'cancelColor',
+              success:(res)=>{
+                  if(res.confirm){
+                      cart.splice(index,1)//在index的地方开始删除一个数据
+                      this.setCart(cart);//更新数据
+                  }
+              }
+            })
+        } else {
+            cart[index].num += operation; //加上1或-1
+            //   重新计算
+            this.setCart(cart);
+        }
 
     },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
+    //点击结算功能
+    handlePay(){
+        const {address,totalNum} = this.data;
+        if(!address){
+            wx.showToast({
+              title: '您还没有选择收货地址',
+              icon: 'none',
+            })
+            return;
+        }
+        if(totalNum===0){
+            wx.showToast({
+              title: '您还没有选择要购买的商品',
+              icon: 'none',
+            })
+            return;
+        }
+        wx.navigateTo({
+          url: '/pages/pay/index',//跳转到支付页面
+        })
     },
+    //设置购物车状态 重新计算底部工具栏 全选 总结 总数量 重新设置缓存
+    setCart(cart) {
+        let allChecked = true;
+        let totalPrice = 0;
+        let totalNum = 0;
+        // 遍历cart中所有商品复选框的checked状态
+        cart.forEach(v => {
+            if (v.checked) {
+                // console.log(v.checked,allChecked);
+                // 计算汇总购物车的商品数量
+                totalNum += v.num;
+                // 计算汇总购物车的商品总价
+                totalPrice += v.price * v.num;
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+            } else {
+                allChecked = false;
+            }
+        })
+        allChecked = cart.length != 0 ? allChecked : false;
+        console.log(allChecked);
+        this.setData({
+            cart,
+            allChecked,
+            totalNum,
+            totalPrice,
+        })
+        // cart设置到缓存重
+        wx.setStorageSync('cart', cart)
     }
+
 })
--- pages/product_detail/index.js
+++ pages/product_detail/index.js
@@ -3,9 +3,9 @@
 import {
     getBaseUrl,
     requestUtil
-  } from '../../utils/requestUtil.js'
-  // 引入 facebook的regenerator库
-  import regeneratorRuntime from '../../lib/runtime/runtime.js'
+} from '../../utils/requestUtil.js'
+// 引入 facebook的regenerator库
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 Page({
 
     /**
@@ -13,11 +13,11 @@ Page({
      */
     data: {
         baseUrl: '',
-        productObj:{},
-        activeIndex:0
+        productObj: {},
+        activeIndex: 0
 
     },
-
+    productInfo: {},
     /**
      * 生命周期函数--监听页面加载
      */
@@ -25,20 +25,74 @@ Page({
         // console.log(options.id);//获取id
         const baseUrl = getBaseUrl();
         this.setData({
-          baseUrl
+            baseUrl
         });
         this.getProductDetail(options.id)
     },
     /**
      * tab点击切换
      */
-    handleItemTap(e){
-        const {index} = e.currentTarget.dataset;
+    handleItemTap(e) {
+        const { index } = e.currentTarget.dataset;
         console.log(index);
         this.setData({
-            activeIndex:index
+            activeIndex: index
         })
     },
+
+    /**
+ * 获取商品详情 
+ */
+    async getProductDetail(id) {
+        const result = await requestUtil({
+            url: '/product/detail',
+            data: { id },
+            // method: "get"
+        });
+        console.log(result);
+        this.productInfo = result.message //存储当前的商品信息 用于购物车的操作
+        const baseUrl = getBaseUrl();
+        this.setData({
+            productObj: result.message,
+            baseUrl
+        })
+    },
+    /**
+     * 点击事件：商品加入购物车
+     */
+    handleCart() {
+        this.setCartadd();
+        wx.showToast({
+            title: '加入成功',
+            icon: "success",
+            mask: true
+        })
+    },
+    // 点击立即购买
+    handleBuy() {
+        this.setCartadd();
+        wx.switchTab({
+          url: '/pages/cart/index',
+        })
+    },
+    //加入购物车
+    setCartadd() {
+        //  获取缓存中的购物车  获取不到变为n空，初始化为数值
+        let cart = wx.getStorageSync('cart') || [];
+        // 判断商品是否在购物车里面 (无：返回-1，有返回该商品下标)
+        let index = cart.findIndex(v => v.id === this.productInfo.id);
+        if (index === -1) { //购物车里面不存在当前商品
+            this.productInfo.num = 1;//添加一个元素num
+            this.productInfo.checked = true;//添加checked属性，默认选择商品
+            cart.push(this.productInfo);
+        } else {//购物车里面存在当前商品
+            cart[index].num++;
+        }
+        wx.setStorageSync('cart', cart);//把购物车里的商品数据添加到缓存中
+    },
+
+
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -88,17 +142,5 @@ Page({
 
     },
 
-    /**
-   * 获取商品详情 
-   */
-    async getProductDetail(id){
-        const result = await requestUtil({
-            url:'/product/detail',
-            data:{id},
-            method:"get"
-        });
-        this.setData({
-            productObj:result.message
-        })
-    }
+
 })
