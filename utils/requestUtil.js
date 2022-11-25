02a1c9610d456fb57ac2fff40c8dd0e37ffe6f3f
LinKitten <2634024527@qq.com>
2022-11-25 20:45:36 +0800

    end

--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -1,8 +1,9 @@
 /**
  * 定义请求根路径 baseUrl
  */
-// const beasUrl = "http://10.225.83.228:8080";
-const beasUrl = "http://localhost:8080";
+const beasUrl = "http://10.225.83.228:8080";
+// const beasUrl = "http://localhost:8080";
+// const beasUrl = "http://192.168.75.238:8080";
 
 //同时并发请求的次数
 // let ajaxTimes = 0;
@@ -11,11 +12,46 @@ const beasUrl = "http://localhost:8080";
 export const getBaseUrl = () => {
     return beasUrl;
 }
+// wx login封装
+export const getWxLogin = () => {
+    return new Promise((resolve, reject) => {
+        wx.login({
+            timeout: 5000,//最长等待时间
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
+    });
+}
+// wx getUserProfile封装
+export const getUserProfile = () => {
+    return new Promise((resolve, reject) => {
+        wx.getUserProfile({
+            desc: '获取用户信息',
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
+    });
+}
 /**
  * 后端请求工具类
  */
 export const requestUtil = (params) => {
+    // 判断url中是否带有 /my/ 请求的是私有路径 带上header token
+    let header = { ...params.header };
+    if (params.url.includes("/my/")) {
+        // 拼接header 带上token
+        header["token"] = wx.getStorageSync('token');
+    }
 
+    //#region 
     // var start = new Date().getTime();
     // console.log("ajaxTimes=" + ajaxTimes)
     // ajaxTimes++;
@@ -28,10 +64,12 @@ export const requestUtil = (params) => {
     // while (true) {
     //     if (new Date().getTime()-start>3*1000) break;
     // };
+    //#endregion
 
     return new Promise((resolve, reject) => {
         wx.request({
             ...params,//解构url
+            header,
             url: beasUrl + params.url,//请求url更改
             success: (result) => {
                 resolve(result.data)
@@ -46,6 +84,23 @@ export const requestUtil = (params) => {
             //         wx.hideLoading()
             //     }
             // }
+        })
+    });
+}
+
+/**
+* promise形式的 小程序的微信支付
+*/
+export const requestPay = (pay) => {
+    return new Promise((resolve, reject) => {
+        wx.requestPayment({
+            ...pay,
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
         })
     });
 }
